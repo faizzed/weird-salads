83f32535d3e605e5ab166ce9eb80827f6fc61d09
faizzed <faiz.qayyum@intercom.io>
2024-01-27 10:10:57 +0000

    setup routing for order and ingredients

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :setup_app_data
+
+  def setup_app_data
+    @data = {
+      appName: "Weird Salads",
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "salads#index"
 
   resources :salads, only: [:index, :create]
+  resources :ingredients, only: [:index, :create, :update]
+  resources :orders, only: [:index, :create]
 end
