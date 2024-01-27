7e143a1080296af97cde6ae85f6d05f94253205d
faizzed <faiz.qayyum@intercom.io>
2024-01-27 19:30:22 +0000

    update selling logic

--- app/commands/create_order.rb
+++ app/commands/create_order.rb
@@ -13,10 +13,12 @@ class CreateOrder
     end
 
     ActiveRecord::Base.transaction do
-
       @order = Order.create!(salad: @salad, price: @salad.price)
 
-      # @salad.salad_ingredients.each { |si| si.ingredient.update!(quantity: si.ingredient.quantity - si.quantity) }
+      @salad.salad_ingredients.each do |salad_ingredient|
+        ingredient = salad_ingredient.ingredient
+        ingredient.update!(quantity: ingredient.quantity - salad_ingredient.quantity)
+      end
     end
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,6 @@
 class Ingredient < ApplicationRecord
   include ActiveModel::Serialization
 
-  validates :name, presence: true, uniqueness: true
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than: 0 }
 end
