d98bcee76bbb9d6f94f196f7ab0382b5ae522123
faizzed <faiz.qayyum@intercom.io>
2024-01-27 09:16:20 +0000

    create salad model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_30_202331) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_27_091253) do
+  create_table "salads", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
